0a01c914ff475b3b7750f5db816249fddf7a1437
galaxy4srinu <galaxy4srinu@gmail.com>
2022-05-19 09:54:37 +0530

    Updated searchPlans Method

--- src/main/java/in/ashokit/service/ReportServiceImpl.java
+++ src/main/java/in/ashokit/service/ReportServiceImpl.java
@@ -40,26 +40,29 @@ public class ReportServiceImpl implements ReportService {
 			eligRecords = repository.findAll();
 
 		} else {
-			EligibilityDtlsEntity entity = new EligibilityDtlsEntity();
 
 			String planName = request.getPlanName();
 			String planStatus = request.getPlanStatus();
 			LocalDate startDate = request.getStartDate();
 			LocalDate endDate = request.getEndDate();
 
-			if (planName != null && !request.getPlanName().equals("")) {
+			EligibilityDtlsEntity entity = new EligibilityDtlsEntity();
+
+			if (planName != null && !planName.equals("")) {
 				entity.setPlanName(planName);
 			}
-			if (planStatus != null && !request.getPlanStatus().equals("")) {
+			if (planStatus != null && !planStatus.equals("")) {
 				entity.setPlanStatus(planStatus);
 			}
 			if (startDate != null && endDate != null) {
 				entity.setStartDate(startDate);
 				entity.setEndDate(endDate);
 			}
+			System.out.println(entity.toString());
 
 			Example<EligibilityDtlsEntity> of = Example.of(entity);
 			eligRecords = repository.findAll(of);
+
 		}
 
 		List<SearchResponse> response = new ArrayList<SearchResponse>();
@@ -75,10 +78,10 @@ public class ReportServiceImpl implements ReportService {
 	private boolean validateRequest(SearchRequest request) {
 		boolean isEmpty = true;
 
-		if (request.getPlanName() != null && request.getPlanName().equals("")) {
+		if (request.getPlanName() != null && !request.getPlanName().equals("")) {
 			return isEmpty = false;
 		}
-		if (request.getPlanStatus() != null && request.getPlanStatus().equals("")) {
+		if (request.getPlanStatus() != null && !request.getPlanStatus().equals("")) {
 			return isEmpty = false;
 		}
 		if (request.getStartDate() != null && request.getEndDate() != null) {
